5cde5baa76cf9af8075053407fea2b67ded1a881
jeff le <1761414433@qq.com>
2023-09-23 12:35:42 +0800

    框架搭建

--- backend/kob/src/main/java/com/kob/backend/controller/pk/botinfoController.java
+++ backend/kob/src/main/java/com/kob/backend/controller/pk/botinfoController.java
@@ -1,13 +1,19 @@
 package com.kob.backend.controller.pk;
 
+import javafx.fxml.FXMLLoader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/pk/")
 public class botinfoController {
     @RequestMapping("bot/")
-    public String bot(){
-        return "hello bot";
+    public Map<String, String> botRaing(){
+        Map<String, String> bot1 = new HashMap<String, String>();
+        bot1.put("name", "zz");
+        return bot1;
     }
 }
---
+++ webb/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div>bot_name:{{ bot_name }}</div>
+    <div>bot_fig:{{ bot_rating }}</div>
+  </div>
+  <router-view></router-view>
+</template>
+
+<script>
+import $ from 'jquery';
+import {ref} from 'vue';
+
+export default{
+  name:"app",
+  setup:() => {
+    let bot_name = ref("");
+    let bot_rating = ref("");
+
+    $.ajax({
+      url: "http://localhost:3000/pk/bot/",
+      type: "get",
+      success: resp => {
+        bot_name.value = resp.name;
+      }
+    } 
+    );
+
+    return{
+      bot_name,
+      bot_rating
+    }
+  }
+}
+</script>
+
+<style>
+</style>
