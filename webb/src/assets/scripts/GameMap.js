ba12ff037a4d5c8e74bb2bb1e3bdd78d01edef66
jeff le <1761414433@qq.com>
2023-09-25 17:14:51 +0800

    实现蛇及输入

--- webb/src/assets/scripts/GameMap.js
+++ webb/src/assets/scripts/GameMap.js
@@ -1,5 +1,6 @@
 import { Game_Object } from "./GameObject";
 import { Wall } from "./Wall";
+import { Snake } from "./Snake";
 
 export class GameMap extends Game_Object{
     constructor(ctx, parent){
@@ -10,9 +11,28 @@ export class GameMap extends Game_Object{
 
         this.L = 0;
         this.rows = 13;
-        this.cols = 13;
+        this.cols = 14;
         this.inner_walls_nums = 30;
         this.walls = [];
+
+        this.snakes = [
+            new Snake({
+                id: 0,
+                color: "#4876EC",
+                r: this.rows - 2,
+                c: 1,
+            }, this),
+            new Snake({
+                id: 1,
+                color: "#F94848",
+                r: 1,
+                c: this.cols - 2,
+            }, this),
+        ];
+
+        this.eye_direction = 0;
+        if(this.id === 1) this.eye_direction = 2;
+        
     }
 
     check_connect(g, sx, sy, tx, ty){
@@ -38,9 +58,9 @@ export class GameMap extends Game_Object{
         }
 
         for(let r = 0; r < this.rows; r ++){
-            g[r][0] = g[r][this.rows - 1] = true;
+            g[r][0] = g[r][this.cols - 1] = true;
         }
-        for(let c = 0; c < this.rows; c ++){
+        for(let c = 0; c < this.cols; c ++){
             g[0][c] = g[this.rows - 1][c] = true;
         }
 
@@ -48,11 +68,11 @@ export class GameMap extends Game_Object{
             for(let j = 0; j < 1000; j ++){
                 let c = parseInt(Math.random() * this.cols);
                 let r = parseInt(Math.random() * this.rows);
-                if(g[r][c] || g[c][r]) continue;
+                if(g[r][c] || g[this.rows - 1 - r][this.cols - 1 - c]) continue;
                 if(r == this.rows - 2 && c == 1 || r == 1 && c == this.cols - 2)
                     continue;
 
-                g[r][c] = g[c][r] = true;
+                    g[r][c] = g[this.rows - 1 - r][this.cols - 1 - c] = true;
                 break;
             }
         }
@@ -70,10 +90,27 @@ export class GameMap extends Game_Object{
         return true;
     }
 
+    add_listening_events(){
+        this.ctx.canvas.focus();
+
+        const [snake0, snake1] = this.snakes;
+        this.ctx.canvas.addEventListener("keydown", e => {
+            if (e.key === 'w') snake0.set_direction(0);
+            else if (e.key === 'd') snake0.set_direction(1);
+            else if (e.key === 's') snake0.set_direction(2);
+            else if (e.key === 'a') snake0.set_direction(3);
+            else if (e.key === "ArrowUp") snake1.set_direction(0);
+            else if (e.key === "ArrowRight") snake1.set_direction(1);
+            else if (e.key === "ArrowDown") snake1.set_direction(2);
+            else if (e.key === "ArrowLeft") snake1.set_direction(3);
+        });
+    }
+
     start(){
         for(let i = 0; i < 1000; i ++)
             if(this.creat_walls())
                 break;
+        this.add_listening_events();
     }
 
     update_size(){
@@ -82,10 +119,47 @@ export class GameMap extends Game_Object{
         this.ctx.canvas.height = this.L * this.rows;
     }
 
+    check_ready(){
+        for(const snake of this.snakes){
+            if (snake.status !== "idle") return false;
+            if (snake.direction === -1) return false;
+        }
+        return true;
+    }
+
+    check_valid(cell){
+        for(const wall of this.walls){
+            if(wall.r === cell.r && wall.c === cell.c)
+                return false;
+        }
+
+        for(const snake of this.snakes){
+            let k = snake.cells.length;
+            if(!snake.check_tail_increase()){
+                k --;
+            }
+            for(let i = 0; i < k; i ++){
+                if(snake.cells[i].r === cell.r && snake.cells[i].c === cell.c){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    next_step(){
+        for (const snake of this.snakes) {
+            snake.next_step();
+        }
+
+    }
+
     update(){
         this.update_size();
+        if (this.check_ready()) {
+            this.next_step();
+        }
         this.render();
-
     }
 
     render(){
